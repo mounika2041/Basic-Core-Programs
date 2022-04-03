6da6f356a706a4e403fb39f131024303b3fd8899
duvvamounika <mounikaduvva05@gmail.com>
2022-04-03 10:48:12 +0530

    add LeapYearBCP2 branch and program

--- Basic-Core-Program/Program.cs
+++ Basic-Core-Program/Program.cs
@@ -6,32 +6,18 @@ using System.Threading.Tasks;
 
 namespace Basic_Core_Program
 {
-    class FlipCoin
-    {
-        public static void FindFlipCoinCount()
+    class LeapYear
+    { 
+       
+        static void Main(string[] args)
         {
-            int tailcount = 0, headcount = 0;
-            Random random = new Random();
-            Console.WriteLine("enter number of times to flipcoin");
-            int numberofTimes = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i < numberofTimes; i++)
-            {
-                double input = random.NextDouble();
-                if (input < 0.5)
-                    tailcount++;
-                else
-                    headcount++;
-            }
-            Console.WriteLine("headcount:{0} tailcount:{1}", headcount, tailcount);
-            double headPercentage = (headcount * 100) / numberofTimes;
-            double tailPercentage = (tailcount * 100) / numberofTimes;
-            Console.WriteLine("Head percentage:{0}% Tail percentage:{1}%", headPercentage, tailPercentage);
-        }
-
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("welcome to basic core programs");
-            FlipCoin.FindFlipCoinCount();
+            int year;
+            Console.WriteLine("enter a year");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+                Console.WriteLine("year is a leap year");
+            else
+                Console.WriteLine("year is not a leap year");
             Console.ReadLine();
         }
     }
