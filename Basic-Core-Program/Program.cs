e97e127e867d4053850282665a134c2c671ba181
duvvamounika <mounikaduvva05@gmail.com>
2022-04-03 10:25:30 +0530

    welcome program

---
+++ Basic-Core-Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to basic core programs");
+            Console.ReadLine();
+        }
+    }
+}
