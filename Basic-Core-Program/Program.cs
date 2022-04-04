4d1125d3cc173ce5a26e3acfcc4948712d0c40f1
duvvamounika <mounikaduvva05@gmail.com>
2022-04-04 20:19:25 +0530

    Add HarmonicSeriesUC4 branch and program

--- Basic-Core-Program/Program.cs
+++ Basic-Core-Program/Program.cs
@@ -6,25 +6,25 @@ using System.Threading.Tasks;
 
 namespace Basic_Core_Program
 {
-   class Factors
+   class HarmonicSeries
    {
       
         static void Main(string[] args)
         {
-            int n, i;
-            Console.WriteLine("enter a integer number");
-            n = int.Parse(Console.ReadLine());
-            while(n!=1)
-                for(i=2;i<=n;i++)
-                {
-                    if(n%i==0)
-                    {
-                        Console.WriteLine(i + "is a factor of" + n);
-                        n = n / i;
-                        break;
-                    }
-                }
+            int  num;
+            Console.WriteLine("The harmonic series is 1/1+1/2+1/3+1/4+.......+1/n");
+            Console.WriteLine("================================");
+            Console.WriteLine("enter the value of N");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (i<=num)
+
+                    Console.WriteLine("1/{0} +", i);
+                
+            }
             Console.ReadLine();
         }
+
     }
 }
